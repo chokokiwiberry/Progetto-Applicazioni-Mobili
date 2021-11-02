3a8c8ee1d7c28baaa8f7b628f727f88565a8511e
chokokiwiberry <kusamdevilal@gmail.com>
2021-11-02 13:02:24 +0100

    camera permission

--- app/src/main/java/com/example/prova1progetto/CreateProduct.java
+++ app/src/main/java/com/example/prova1progetto/CreateProduct.java
@@ -1,11 +1,19 @@
 package com.example.prova1progetto;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Switch;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,6 +26,8 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class CreateProduct  extends AppCompatActivity {
+    private static final int REQUEST_IMAGE_CAPTURE = 0;
+    private static final int MY_CAMERA_REQUEST_CODE = 100;
     private Button savebutton;
     private EditText nametext;
     private EditText descriptiontext;
@@ -25,7 +35,36 @@ public class CreateProduct  extends AppCompatActivity {
     private String received_token;
     private String received_sessiontoken;
     private String received_barcode;
+    private Button takephoto;
     private LAM_Api lam_api;
+
+    static final int REQUEST_TAKE_PHOTO = 123;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            // Sample data cast to  thumbnail
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            // Do whatever you want with the thumbnail such as setting it to image view
+            ImageView mImageView = null;
+            mImageView.setImageBitmap(imageBitmap);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == MY_CAMERA_REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "camera permission granted", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "camera permission denied", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_createproduct);
@@ -33,7 +72,7 @@ public class CreateProduct  extends AppCompatActivity {
         descriptiontext = (EditText) findViewById(R.id.id_description);
         savebutton = (Button) findViewById(R.id.id_saveproduct);
         testswitch = (Switch) findViewById(R.id.id_test);
-
+        takephoto = (Button) findViewById(R.id.id_takephoto);
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
@@ -51,28 +90,33 @@ public class CreateProduct  extends AppCompatActivity {
         savebutton.setOnClickListener(v -> {
             saveProduct();
         });
+
+        takephoto.setOnClickListener(v -> {
+            takePhoto();
+        });
     }
 
     private void saveProduct() {
+
 
         try {
             HashMap<String, Object> postData = new HashMap<>();
             postData.put("token", received_sessiontoken);
             postData.put("name", this.nametext.getText().toString());
             postData.put("description", this.descriptiontext.getText().toString());
-            postData.put("barcode", received_barcode);
-            postData.put("test", testswitch.isChecked());
-            Log.d("diamond", "sto per essere impachettato: "+postData);
+            postData.put("barcode", received_barcode); //credo che non farò inserire il barcode dall'utente - che venga letto da camera o meno
+            postData.put("test", testswitch.isChecked()); //quando ci sarà la versione finale, questo sarà sempre di default falso
+            Log.d("diamond", "sto per essere impachettato: " + postData);
             Call<Product> call = lam_api.postProduct("Bearer " + received_token, postData);
             call.enqueue(new Callback<Product>() {
                 @Override
                 public void onResponse(Call<Product> call, Response<Product> response) {
                     Log.d("cibo", "response");
                     if (!response.isSuccessful()) {
-                        Log.d("diamond","you did but didnt work "+ String.valueOf(response));
+                        Log.d("diamond", "you did but didnt work " + String.valueOf(response));
                     }
                     Log.d("diamond", String.valueOf(response.body()));
-                    Log.d("diamond", "anananananannanana "+ response.code());
+                    Log.d("diamond", "anananananannanana " + response.code());
 
                 }
 
@@ -86,5 +130,15 @@ public class CreateProduct  extends AppCompatActivity {
             e.printStackTrace();
         }
 
+    }
+
+    private void takePhoto() {
+        if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_REQUEST_CODE);
+        }
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+        }
     }
 }
