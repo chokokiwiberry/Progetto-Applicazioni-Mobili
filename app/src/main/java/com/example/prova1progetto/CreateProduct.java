80bee0120c8796313230f4e9ab2cd67137d59a4f
chokokiwiberry <kusamdevilal@gmail.com>
2021-11-05 11:41:55 +0100

    camera gallery working

--- app/src/main/java/com/example/prova1progetto/CreateProduct.java
+++ app/src/main/java/com/example/prova1progetto/CreateProduct.java
@@ -18,6 +18,10 @@ import android.widget.Switch;
 import android.widget.Toast;
 
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -38,21 +42,26 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+
 public class CreateProduct extends AppCompatActivity {
 
-    private Button savebutton, savepic;
+    private Button savebutton, gallerybutton;
+
     private EditText nametext;
     private EditText descriptiontext;
     private Switch testswitch;
     private String received_token;
     private String received_sessiontoken;
     private String received_barcode;
-    private Button takephoto;
+    private Button takephotobutton;
     private LAM_Api lam_api;
-    private static final int MY_CAMERA_REQUEST_CODE = 100;
     private ImageView imageView;
 
-    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private static final int CAMERA_PERM_CODE = 101;
+
+    ActivityResultLauncher<String> getGalleryContent;
+    ActivityResultLauncher<Intent> launchCamera;
 
 
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -62,9 +71,10 @@ public class CreateProduct extends AppCompatActivity {
         descriptiontext = (EditText) findViewById(R.id.id_description);
         savebutton = (Button) findViewById(R.id.id_saveproduct);
         testswitch = (Switch) findViewById(R.id.id_test);
-        takephoto = (Button) findViewById(R.id.id_takephoto);
-        savepic = (Button) findViewById(R.id.id_savepic);
+        takephotobutton = (Button) findViewById(R.id.id_takephoto);
+        gallerybutton = (Button) findViewById(R.id.id_gallery);
         imageView = findViewById(R.id.id_imageview);
+
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
@@ -82,83 +92,40 @@ public class CreateProduct extends AppCompatActivity {
         savebutton.setOnClickListener(v -> {
             saveProduct();
         });
-        takephoto.setOnClickListener(v -> {
+
+        launchCamera = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+                            Log.d("diamond", "credo che non arrivi qui");
+                            Bundle bundle = result.getData().getExtras();
+                            Bitmap bitmap = (Bitmap) bundle.get("data");
+                            imageView.setImageBitmap(bitmap);
+                        }
+                    }
+                });
+
+        getGalleryContent = registerForActivityResult(new ActivityResultContracts.GetContent(), new ActivityResultCallback<Uri>() {
+            @Override
+            public void onActivityResult(Uri result) {
+                imageView.setImageURI(result);
+            }
+        });
+
+        takephotobutton.setOnClickListener(v -> {
             askPermission();
         });
-        savepic.setOnClickListener(v->{
-            dispatchTakePictureIntent();
+
+        gallerybutton.setOnClickListener(v -> {
+            getGalleryContent.launch("image/*");
         });
 
 
     }
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        // Ensure that there's a camera activity to handle the intent
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            // Create the File where the photo should go
-            File photoFile = null;
-            try {
-                photoFile = createImageFile();
-            } catch (IOException ex) {
-                // Error occurred while creating the File
-
-            }
-            // Continue only if the File was successfully created
-            if (photoFile != null) {
-                Uri photoURI = FileProvider.getUriForFile(this,
-                        "com.example.android.fileprovider",
-                        photoFile);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-            }
-        }
-    }
-
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            //Bundle extras = data.getExtras();
-            //Bitmap imageBitmap = (Bitmap) extras.get("data");
-            //imageView.setImageBitmap(imageBitmap);
-           // galleryAddPic();
-            Bitmap captureImage = (Bitmap) data.getExtras().get("data");
-            imageView.setImageBitmap(captureImage);
-        }
-    }
-
-    private void galleryAddPic() {
-        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-        File f = new File(currentPhotoPath);
-        Uri contentUri = Uri.fromFile(f);
-        mediaScanIntent.setData(contentUri);
-        this.sendBroadcast(mediaScanIntent);
-    }
-
-    String currentPhotoPath;
-
-    private File createImageFile() throws IOException {
-        // Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
-
-        // Save a file: path for use with ACTION_VIEW intents
-        currentPhotoPath = image.getAbsolutePath();
-        return image;
-    }
-
 
 
     private void saveProduct() {
-
-
         try {
             Map<String, Object> postData = new HashMap<>();
             postData.put("token", received_sessiontoken);
@@ -177,7 +144,7 @@ public class CreateProduct extends AppCompatActivity {
                     }
                     Log.d("diamond", String.valueOf(response.body()));
                     Log.d("diamond", "anananananannanana " + response.code());
-                    if (response.code() == 201){
+                    if (response.code() == 201) {
                         //faccio partire un messaggio con l'anteprima oppure messaggio ok
                         //reinderizzamento
                     }
@@ -195,33 +162,16 @@ public class CreateProduct extends AppCompatActivity {
 
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == 5) {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                takePhotoFromCamera();
-            }
-
-        }
-    }
-
-
-    private void takePhotoFromCamera() {
-        Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-        if (intent.resolveActivity(getPackageManager()) != null)
-            startActivity(intent);
+    private void openCamera() {
+        Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            launchCamera.launch(camera);
     }
 
     private void askPermission() {
-        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA)
-                == PackageManager.PERMISSION_DENIED) {
-            Toast.makeText(this, "Boh perché", Toast.LENGTH_SHORT).show();
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 5);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, CAMERA_PERM_CODE);
         } else {
-            Toast.makeText(this, "else voglio vedere cos'è ", Toast.LENGTH_SHORT).show();
-            takePhotoFromCamera();
+            openCamera();
         }
-
     }
 }
